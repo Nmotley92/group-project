6fb60eae507740f11359b3d892d38cd5a4647629
Nathaniel Motley <nmotley92@gmail.com>
2022-12-01 23:02:07 -0600

    changed form so it is responsive also made it so it clears and local events on city search

--- assets/Geolocator.js
+++ assets/Geolocator.js
@@ -31,22 +31,40 @@ function createLocalEventsGeo(lat, long) {
                 modal.css('display', 'block')
                 return;
             }
+            var formObject = {
+                key: "",
+                date: "",
+                eventTitle: "",
+                time: "",
+                details: ""
+            };
 
             for (let i = 0; i < data.events.length; i++) {
                 // creats a clickable image
                 var containerImgEl = $('<div>');
                 var imageEL = $('<img>');
-                var aEL = $('<a>');
+                // var aEL = $('<a>');
                 var thirdDivEl = $('<div>');
                 var foruthDivEl = $('<div>');
                 var fifthDiveEl = $('<div>');
                 var h5El = $('<h5>');
                 var pel = $('<p>');
+                var newButton = $('<a>');
+
                 // pulls and assigns data from fetch
                 var imgSrc = data.events[i].performers[0].image;
                 var link = data.events[i].url;
-                var title = data.events[i].title + " " + data.events[0].type;
+                var title = data.events[i].title + " " + data.events[i].type;
                 var time = new Date(data.events[i].datetime_local).toLocaleString();
+                var time2 = new Date(data.events[i].datetime_local).getDate();
+                var time3 = new Date(data.events[i].datetime_local).toLocaleTimeString();
+                var month = new Date(data.events[i].datetime_local).getMonth()+1;
+                var copyButtonEl = $('<button>');
+                copyButtonEl.addClass('copy float-right w-auto bg-grey-500 rounded-md mb-1 px-4 py-1 text-black-50 shadow-black-200 dark:shadow-none text-center font-bold hover:showdow-none ring');
+                copyButtonEl.text("Add to calendar");
+                copyButtonEl.attr("value", title);
+                copyButtonEl.attr("id", "key-"+time2);
+
 
 
                 // adds classes for the image container
@@ -58,28 +76,70 @@ function createLocalEventsGeo(lat, long) {
                 thirdDivEl.addClass("absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed");
                 thirdDivEl.attr("style", "background-color: rgba(0, 0, 0, 0.4)");
                 foruthDivEl.addClass("flex justify-start items-end h-full");
-                fifthDiveEl.addClass("text-white m-6");
-                h5El.addClass('font-bold text-lg mb-3');
+                fifthDiveEl.addClass("w-full text-white m-6");
+                h5El.addClass(' font-bold text-lg mb-3');
                 pel.addClass('font-bold text-lg mb-3');
                 // adds src and href
                 imageEL.attr("src", imgSrc);
-                aEL.attr("href", link);
+                newButton.attr("href", link);
+                newButton.attr("target", "_blank");
+                newButton.addClass("w-auto float-left bg-grey-500 rounded-md mb-1 px-4 py-1 text-black-50 shadow-black-200 dark:shadow-none text-center font-bold hover:showdow-none ring")
                 //  puts the text for title and time
                 h5El.text(title);
                 pel.text(time);
+                newButton.text("Buy tickets");
                 // adds all the above to page
+
                 localEventCont.append(containerImgEl);
                 containerImgEl.append(imageEL);
-                containerImgEl.append(aEL);
-                aEL.append(thirdDivEl);
+                // containerImgEl.append(aEL);
+
+                containerImgEl.append(thirdDivEl);
                 thirdDivEl.append(foruthDivEl);
                 foruthDivEl.append(fifthDiveEl);
                 fifthDiveEl.append(h5El);
                 fifthDiveEl.append(pel);
+                fifthDiveEl.append(newButton);
+                fifthDiveEl.append(copyButtonEl);
+                formObject.eventTitle = title;
+                formObject.time = time3;
+                formObject.date= "#key-"+time2;
+                formObject.key = title;
+                localStorage.setItem(title, JSON.stringify( formObject));
+                if(month>currentMonth){
+                    copyButtonEl.hide();
+                }
+
             }
+            // listen event for adding local events to the calendar
+            $('.copy').click((event) => {
+                var formObject = JSON.parse(localStorage.getItem(event.target.value));
+                formArray.push(formObject);
+                localStorage.setItem("allButtons", JSON.stringify(formArray));
+                var eventButton = $('<button>');
+                eventButton.addClass('x w-full bg-grey-500 rounded-md mb-1 px-4 py-1 text-black-50 shadow-black-200 dark:shadow-none text-center font-bold hover:showdow-none ring');
+                eventButton.attr('id', event.target.id);
+                eventButton.text(event.target.value);
+                eventButton.attr('value', event.target.value);
+                $("#" +event.target.id).append(eventButton);
+                // formArray.push(formObject);
+                
+                var buttutu = $('.x');
+                // click listener for new buttons
+                buttutu.click((event) => {
+                    var newForm = JSON.parse(localStorage.getItem(event.target.value));
+                    saveForm.hide();
+                    eventName.val(newForm.eventTitle);
+                    eventTime.val(newForm.time);
+                    eventDetails.val(newForm.details);
+                    modal2El.css('display', 'block');
+                })
+              
+        
+
+            })
 
         });
-
 
 }
 
